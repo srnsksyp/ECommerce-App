0654779191f84c801240a5a1db940e8ab945a5b0
Saransh Kashyap <saranshk0511@gmail.com>
2025-07-17 23:43:03 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Order from "./pages/Order";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] xl:px-[11vw] 2xl:px-[13vw]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:produtId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/order" element={<Order />} />
+      </Routes>
+    </div>
+  )
+}
